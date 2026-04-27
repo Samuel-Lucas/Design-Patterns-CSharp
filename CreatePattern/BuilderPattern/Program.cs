d34b09c3b8a87cf37aa6163fe534100bef866b39
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-04-27 15:10:46 -0300

    Implementing examples of Builder pattern

--- CreatePattern/BuilderPattern/Program.cs
+++ CreatePattern/BuilderPattern/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BuilderPattern.Builder;
+
+HamburguerBuilder hamburguerBuilder = new();
+
+// x-salad
+var xSalad = hamburguerBuilder
+    .SetBreadType("white")
+    .SetMeatType("red")
+    .HasSalad()
+    .Build();
+
+// x-bacon
+hamburguerBuilder = new();
+var xBacon = hamburguerBuilder
+    .SetBreadType("black")
+    .SetMeatType("red")
+    .HasBacon()
+    .Build();
+
+// x-all
+hamburguerBuilder = new();
+var xAll = hamburguerBuilder
+    .SetBreadType("white")
+    .SetMeatType("red")
+    .HasBacon()
+    .HasChips()
+    .HasSalad()
+    .Build();
+
+Console.WriteLine(xSalad.getDescription());
+Console.WriteLine("**********************");
+Console.WriteLine(xBacon.getDescription());
+Console.WriteLine("**********************");
+Console.WriteLine(xAll.getDescription());
