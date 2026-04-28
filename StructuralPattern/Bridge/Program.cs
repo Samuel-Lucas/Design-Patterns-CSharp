27836602c80d51e6f2440eaa101d893ee76e308e
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-04-28 20:37:58 -0300

    implementing use case of Bridge pattern

--- StructuralPattern/Bridge/Program.cs
+++ StructuralPattern/Bridge/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Bridge;
+using Bridge.Dialogue;
+
+AbstractWindow window = new WindowDialogue(new LinuxWindow());
+window.Draw();
+
+window = new WindowDialogue(new MacWindow());
+window.Draw();
