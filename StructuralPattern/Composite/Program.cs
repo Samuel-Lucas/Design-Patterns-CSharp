658916ea27fae6a7696468b2ea5c18ccf1421d84
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-05-03 11:50:10 -0300

    Implementing composite

--- StructuralPattern/Composite/Program.cs
+++ StructuralPattern/Composite/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Composite;
+using Composite.Composite;
+
+EmployeeComposite tree = new("Steve jobs");
+EmployeeLeaf employee1 = new("Samuel");
+EmployeeLeaf employee2 = new("Lucas");
+EmployeeComposite employee3 = new("Emanuel");
+EmployeeLeaf employee4 = new("Gabriel");
+EmployeeLeaf employee5 = new("Maria");
+
+tree.Add(employee1);
+tree.Add(employee2);
+employee3.Add(employee4);
+employee3.Add(employee5);
+
+Console.WriteLine(tree.Operation());
+Console.WriteLine(employee3.Operation());
