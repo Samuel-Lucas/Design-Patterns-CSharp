4825513219d0d43b8ab7d07793f79f9b4d9ac5c7
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-05-03 14:19:47 -0300

    Implementing example of decorator pattern

--- StructuralPattern/Decorator/Program.cs
+++ StructuralPattern/Decorator/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Decorator;
+using Decorator.Decorator.Concrete;
+using Decorator.Interfaces;
+
+ICar car = new Car();
+ICar carWithAirConditioner = new AirConditionerDecorator(car);
+ICar carWithLeatherSeat = new AirConditionerDecorator(carWithAirConditioner);
+
+var result = carWithLeatherSeat.GetCar();
+
+Console.WriteLine(result);
