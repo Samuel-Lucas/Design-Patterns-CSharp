e39621fd96653ccf0969546e3ee0de37b12e55cc
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-04 10:51:29 -0300

    Algorithm example of chain of responsibility

--- BehavorialPattern/ChainOfResponsibility/Program.cs
+++ BehavorialPattern/ChainOfResponsibility/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ChainOfResponsibility.Handlers;
+
+Handler handler = new ConcreteHandler();
+Handler handler2 = new ConcreteHandler2();
+Handler handler3 = new ConcreteHandler3();
+
+handler.SetSucessor(handler2);
+handler2.SetSucessor(handler3);
+
+// send requests to chain
+int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20};
+
+foreach (int request in requests)
+{
+    handler.HandleRequest(request);
+}
