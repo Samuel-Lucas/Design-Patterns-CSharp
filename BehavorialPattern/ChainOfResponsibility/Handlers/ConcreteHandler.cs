1d2a66342e2a4c8a770d2fe3bd1032ce2b1ae47f
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-04 10:44:25 -0300

    Chain of responsibility pattern

---
+++ BehavorialPattern/ChainOfResponsibility/Handlers/ConcreteHandler.cs
@@ -0,0 +1,46 @@
+namespace ChainOfResponsibility.Handlers;
+
+public class ConcreteHandler : Handler
+{
+    public override void HandleRequest(int request)
+    {
+        if (request >= 0 && request < 10)
+        {
+            Console.WriteLine($"{this.GetType().Name} handle request {request}");
+        }
+        else if (sucessor != null)
+        {
+            sucessor.HandleRequest(request);
+        }
+    }
+}
+
+public class ConcreteHandler2 : Handler
+{
+    public override void HandleRequest(int request)
+    {
+        if (request >= 10 && request < 20)
+        {
+            Console.WriteLine($"{this.GetType().Name} handle request {request}");
+        }
+        else if (sucessor != null)
+        {
+            sucessor.HandleRequest(request);
+        }
+    }
+}
+
+public class ConcreteHandler3 : Handler
+{
+    public override void HandleRequest(int request)
+    {
+        if (request >= 20 && request < 30)
+        {
+            Console.WriteLine($"{this.GetType().Name} handle request {request}");
+        }
+        else if (sucessor != null)
+        {
+            sucessor.HandleRequest(request);
+        }
+    }
+}
