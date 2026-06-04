a43eacc44920439080cd86d44bd406708eb59379
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-04 15:39:23 -0300

    use case example of the Command pattern

--- BehavorialPattern/Command/Program.cs
+++ BehavorialPattern/Command/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Command;
+using Command.Command;
+
+Receiver receiver = new();
+OpenFileCommand openFileCommand = new(receiver);
+SaveFileCommand saveFileCommand = new(receiver);
+CloseFileCommand closeFileCommand = new(receiver);
+
+Invoker invoker = new();
+invoker.SetCommand(openFileCommand);
+invoker.ExecuteCommand();
+
+invoker.SetCommand(saveFileCommand);
+invoker.ExecuteCommand();
+
+invoker.SetCommand(closeFileCommand);
+invoker.ExecuteCommand();
