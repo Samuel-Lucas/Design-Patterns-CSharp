888ef67a6fd486ded111506a644950d03d179d13
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-07 18:38:45 -0300

    Implementing example of mediator pattern

--- BehavorialPattern/Mediator/Program.cs
+++ BehavorialPattern/Mediator/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Mediator.Entity;
+using Mediator.Interfaces;
+using Mediator.Mediator.ConcreteMediator;
+
+// create chatroom
+IChatRoom chatRoom = new ChatRoom();
+
+// create users
+User user1 = new ChatUser(chatRoom);
+User user2 = new ChatUser(chatRoom);
+User user3 = new ChatUser(chatRoom);
+
+// register users
+chatRoom.Register(user1);
+chatRoom.Register(user2);
+chatRoom.Register(user3);
+
+// send messages
+user1.Send("Hello world !");
+user2.Send("Hi there");
