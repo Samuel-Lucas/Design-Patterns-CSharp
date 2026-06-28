f1cb5bd3af7bd16de06dcbaa1ae39711d5fc8629
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-28 14:55:29 -0300

    Use case example of state design pattern

--- BehavorialPattern/State/Program.cs
+++ BehavorialPattern/State/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using State.Class;
+using State.States;
+
+var context = new Context();
+
+// state A executed
+context.State = new StateA();
+context.Request();
+
+// state B executed
+context.Request();
+
+// state A is executed again
+context.Request();
