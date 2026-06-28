8f234c073653d92db3f3413cd511b357be37f6ae
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-06-28 14:35:20 -0300

    Use case example of observer pattern

--- BehavorialPattern/Observer/Program.cs
+++ BehavorialPattern/Observer/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Observer.Observer;
+using Observer.Subject;
+
+ConcreteSubject subject = new ConcreteSubject();
+ConcreteObserver observer1 = new ConcreteObserver("Observer 1");
+ConcreteObserver observer2 = new ConcreteObserver("Observer 2");
+ConcreteObserver observer3 = new ConcreteObserver("Observer 3");
+
+subject.RegisterObserver(observer1);
+subject.RegisterObserver(observer2);
+subject.RegisterObserver(observer3);
+
+subject.NotifyObservers(42);
+
+subject.RemoveObserver(observer3);
+
+subject.NotifyObservers(84);
+
+Console.ReadLine();
